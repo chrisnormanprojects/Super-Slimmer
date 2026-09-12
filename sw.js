8c0d46650928a4f542f9e68dfa979c0f801de897
chrisnormanprojects <chrisnorman@live.co.uk>
2026-09-12 11:49:04 +0100

    Cache Aldi/Lidl expansion in PWA v7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='super-slimmer-v6';
-const SHELL=['./','./index.html','./styles.css','./reference-data.js','./supermarket-data.js','./supermarket-bridge.js','./app.js','./manifest.json','./icon.svg'];
+const CACHE='super-slimmer-v7';
+const SHELL=['./','./index.html','./styles.css','./reference-data.js','./supermarket-data.js','./aldi-lidl-extra.js','./supermarket-bridge.js','./app.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;if(new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp})));});
